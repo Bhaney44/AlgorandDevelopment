e8bfc280a15051bcc9d1e9c5bf8607d0c1355136
B Haney <43055154+Bhaney44@users.noreply.github.com>
2021-06-21 05:39:49 -0700

    Add files via upload

--- LaylaCoin/main.py
+++ LaylaCoin/main.py
@@ -5,6 +5,8 @@ from algosdk import account, mnemonic
 from algosdk.future.transaction import write_to_file
 from algosdk.future.transaction import AssetConfigTxn, AssetTransferTxn
 from util import sign_and_send, balance_formatter
+from algosdk.future.transaction import AssetTransferTxn
+
 
 # Client
 # client = algod.AlgodClient(algod_token, algod_address)
@@ -23,7 +25,7 @@ def create(passphrase=None):
 	else:
 		write_to_file([txn], "create_coin.txn")
 
-#Creates an unsigned opt-in transaction for the specified asset id and address. 
+# Creates an unsigned opt-in transaction for the specified asset id and address. 
 # Uses current network params.
 def optin(passphrase=None):
 	params = client.suggested_params()
@@ -35,18 +37,32 @@ def optin(passphrase=None):
 		write_to_file([txns], "optin.txn")
 
 # Creates an unsigned transfer transaction for the specified asset id, to the specified address, for the specified amount.
+#def transfer(passphrase=None):
+	#amount = 6000
+	#params = client.suggested_params()
+	#txn = AssetTransferTxn(sender=creator_address, sp=params, receiver=receiver_address, amt=amount, index=asset_id)
+	#if passphrase:
+		#txinfo = sign_and_send(txn, passphrase, client)
+		#formatted_amount = balance_formatter(amount, asset_id, client)
+		#print("Transferred {} from {} to {}".format(formatted_amount, 
+			#creator_address, receiver_address))
+		#print("Transaction ID Confirmation: {}".format(txinfo.get("tx")))
+	#else:
+		#write_to_file([txns], "transfer.txn")
+
 def transfer(passphrase=None):
-	amount = 6000
-	params = client.suggested_params()
-	txn = AssetTransferTxn(sender=creator_address, sp=params, receiver=receiver_address, amt=amount, index=asset_id)
-	if passphrase:
-		txinfo = sign_and_send(txn, passphrase, client)
-		formatted_amount = balance_formatter(amount, asset_id, client)
-		print("Transferred {} from {} to {}".format(formatted_amount, 
-			creator_address, receiver_address))
-		print("Transaction ID Confirmation: {}".format(txinfo.get("tx")))
-	else:
-		write_to_file([txns], "transfer.txn")
+
+    amount = 6000
+    params = client.suggested_params()
+    txn = AssetTransferTxn(sender=creator_address, sp=params, receiver=receiver_address, amt=amount, index=asset_id)
+    if passphrase:
+        txinfo = sign_and_send(txn, passphrase, client)
+        formatted_amount = balance_formatter(amount, asset_id, client)
+        print("Transferred {} from {} to {}".format(formatted_amount, 
+            creator_address, receiver_address))
+        print("Transaction ID Confirmation: {}".format(txinfo.get("tx")))
+    else:
+        write_to_file([txns], "transfer.txn")
 
 # Checks the asset balance for the specific address and asset id.
 def check_holdings(asset_id, address):
@@ -59,3 +75,43 @@ def check_holdings(asset_id, address):
 			return
 	print("Account {} must opt-in to Asset ID {}.".format(address, asset_id))
 
+
+# Creates an unsigned opt-in transaction for the specified asset id and address. 
+# Uses current network params.
+def optin(passphrase=None):
+    params = client.suggested_params()
+    txn = AssetTransferTxn(sender=receiver_address, sp=params, receiver=receiver_address, amt=0, index=asset_id)
+    if passphrase:
+        txinfo = sign_and_send(txn, passphrase, client)
+        print("Opted in to asset ID: {}".format(asset_id))
+    else:
+        write_to_file([txns], "optin.txn")
+	
+from util import balance_formatter
+
+# Checks the asset balance for the specific address and asset id.
+def check_holdings(asset_id, address):
+    account_info = client.account_info(address)
+    assets = account_info.get("assets")
+    for asset in assets:
+        if asset['asset-id'] == asset_id:
+            amount = asset.get("amount")
+            print("Account {} has {}.".format(address, balance_formatter(amount, asset_id, client)))
+            return
+    print("Account {} must opt-in to Asset ID {}.".format(address, asset_id))
+
+
+# Creates an unsigned transfer transaction for the specified asset id, to the specified address, for the specified amount.
+def transfer(passphrase=None):
+
+    amount = 6000
+    params = client.suggested_params()
+    txn = AssetTransferTxn(sender=creator_address, sp=params, receiver=receiver_address, amt=amount, index=asset_id)
+    if passphrase:
+        txinfo = sign_and_send(txn, passphrase, client)
+        formatted_amount = balance_formatter(amount, asset_id, client)
+        print("Transferred {} from {} to {}".format(formatted_amount, 
+            creator_address, receiver_address))
+        print("Transaction ID Confirmation: {}".format(txinfo.get("tx")))
+    else:
+        write_to_file([txns], "transfer.txn")
